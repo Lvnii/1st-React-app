60cffcafdd41eeb7bc66b6511ae83b847ab985fd
Levan Esitashvili <124806271+Lvnii@users.noreply.github.com>
2024-02-29 23:39:46 +0400

    commit

---
+++ my-app/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import logo from './logo.svg';
+import './App.css';
+import Header from './Component/Header/header.jsx'
+import Main from './Component/Main/main.jsx'
+import Footer from './Component/Footer/footer.jsx'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer /> 
+    </>
+  );
+}
+
+export default App;
